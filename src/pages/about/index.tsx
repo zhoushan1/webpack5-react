f88f93d3f88658773394e8d1bb56fdc6bc3f0d3f
周珊 <672563682@qq.com>
2023-01-09 10:56:49 +0800

    feat: 接口测试

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import request from '../../utils/request'
+
+async function fetchData() {
+  return request.get('/api/test')
+}
 
 const About = () => {
   const params = useLocation()
   console.log('路由参数', params)
 
+  const handleClick = async () => {
+    const res = await fetchData().finally(() => {
+      console.log('执行final')
+    })
+  }
+
   return (
     <div>
       <h1>这是about页</h1>
+      <button onClick={handleClick}>测试接口</button>
     </div>
   )
 }
